e9b698aad01420e524a95b139531b410f041c8c8
HackiruGit <95958515+HackiruGit@users.noreply.github.com>
2024-07-09 03:41:17 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Веб-приложение</title>
+</head>
+<body>
+    <h1>Добро пожаловать!</h1>
+    <p>Это уязвимое веб-приложение. Пожалуйста, войдите или зарегистрируйтесь.</p>
+    <a href="register.php">Регистрация</a> |
+    <a href="login.php">Вход</a>
+</body>
+</html>
